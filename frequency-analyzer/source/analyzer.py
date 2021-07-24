1342242312eea6ec03dc62fb650aceba49dee47c
rafaatsouza <souza.t.rafael@gmail.com>
2021-07-24 16:13:56 -0300

    Sets one data frame for each design pattern

--- frequency-analyzer/source/analyzer.py
+++ frequency-analyzer/source/analyzer.py
@@ -13,15 +13,35 @@ class Analyzer:
         if not filepath.endswith('.csv'):
             raise Exception('Invalid filepath')
 
-        self.dataset = pd.read_csv(filepath, sep=';', header=0)
+        self.datasets = self.get_datasets(filepath)
 
-        self.dataset.drop('sistema', inplace=True, axis=1)
-        self.dataset.drop('cbo', inplace=True, axis=1)
-        self.dataset.drop('wmc', inplace=True, axis=1)
-        self.dataset.drop('dit', inplace=True, axis=1)
-        self.dataset.drop('rfc', inplace=True, axis=1)
-        self.dataset.drop('nom', inplace=True, axis=1)
-        self.dataset.drop('loc', inplace=True, axis=1)
+    def get_datasets(self, filepath):
+        dataset = pd.read_csv(filepath, sep=';', header=0)
+
+        dataset.drop('system', inplace=True, axis=1)
+        dataset.drop('cbo', inplace=True, axis=1)
+        dataset.drop('wmc', inplace=True, axis=1)
+        dataset.drop('dit', inplace=True, axis=1)
+        dataset.drop('rfc', inplace=True, axis=1)
+        dataset.drop('nom', inplace=True, axis=1)
+        dataset.drop('loc', inplace=True, axis=1)
+
+        design_patterns = ['Bridge', 'Composite', 'ChainOfResponsibility',
+                           'TemplateMethod', 'FactoryMethod', 'Prototype']
+        datasets = {}
+
+        for design_pattern in design_patterns:
+            datasets[design_pattern] = dataset.loc[dataset[design_pattern] == 1]
+            for _design_pattern in design_patterns:
+                datasets[design_pattern].drop(
+                    _design_pattern, inplace=True, axis=1)
+
+        del design_patterns
+        del dataset
+
+        return datasets
 
     def run(self):
-        print(self.dataset)
+        for key, value in self.datasets.items():
+            print('Dataset for the {} pattern\n'.format(key))
+            print(self.datasets[key])
